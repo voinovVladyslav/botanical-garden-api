ca91b01ec4fda89cba30600a2b4f2fc1a8172614
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-03-19 13:48:14 +0200

    register templates and static folders

--- botanical_garden_root/botanical_garden/settings.py
+++ botanical_garden_root/botanical_garden/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'botanical_garden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + "/botanical_garden/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR + '/botanical_garden/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

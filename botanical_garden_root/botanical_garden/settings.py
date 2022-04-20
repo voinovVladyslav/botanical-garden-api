ac91508f69bf5dba6aa505f61085f82e39cc011b
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-04-20 16:06:43 +0300

    created new app contact

--- botanical_garden_root/botanical_garden/settings.py
+++ botanical_garden_root/botanical_garden/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- botanical_garden_root/pages/urls.py
+++ botanical_garden_root/pages/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # home page
     path('', views.main, name='main'),
     path('history', views.history, name='history'),
     path('structure', views.structure, name='structure'),

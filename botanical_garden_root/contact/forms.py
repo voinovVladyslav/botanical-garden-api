042d0be6182cf8ab6f12535010f4f9420380f31f
underhazee <uh4zee@gmail.com>
2022-04-22 14:44:10 +0300

    form frontend chenges)

--- botanical_garden_root/contact/forms.py
+++ botanical_garden_root/contact/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    user_name = forms.CharField(max_length=100, label="Ім'я: ")
-    phone_number = forms.IntegerField(label = "Номер телефону: ")
-    user_email = forms.EmailField(label="E-Mail: ")
-    user_message = forms.CharField(widget=forms.Textarea, label="Повідомлення: ")
+    user_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': "Ім'я:"}))
+    phone_number = forms.IntegerField(label='', widget=forms.TextInput(attrs={'placeholder': 'Номер телефону:'}))
+    user_email = forms.EmailField(label='', widget=forms.TextInput(attrs={'placeholder': 'E-Mail:'}))
+    user_message = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': 'Повідомлення:'}))

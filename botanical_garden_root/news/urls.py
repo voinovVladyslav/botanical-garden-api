de5d947d58df523e6739f4eb1c1e98a3a593635c
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-04-26 09:42:22 +0300

    created templates for all news and single one

--- botanical_garden_root/news/urls.py
+++ botanical_garden_root/news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# news/
 urlpatterns = [
-    path('', views.news, name='news'),
+    path('all', views.news_all, name='news_all'),
+    path('<str:news_pk>', views.news_single, name='news_single'),
 ]
--- botanical_garden_root/news/views.py
+++ botanical_garden_root/news/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
-def news(request):
-    return render(request, 'news/news.html')
+def news_all(request):
+    news = News.objects.all()
+
+    context = {'news': news}
+    return render(request, 'news/news.html', context=context)
+
+def news_single(request, news_pk):
+    news = News.objects.get(id=news_pk)
+    
+    context = {'news': news}
+    return render(request, 'news/news_single.html', context=context)

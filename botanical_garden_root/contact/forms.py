e518970c372c6e0e93368a2985f3630f3d89010f
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-05-02 20:13:15 +0300

    create model for contract and use modelform instead of form

--- botanical_garden_root/contact/forms.py
+++ botanical_garden_root/contact/forms.py
@@ -1,7 +1,9 @@
-from django import forms
+from dataclasses import field
+from django.forms import ModelForm
+from .models import Contact
 
-class ContactForm(forms.Form):
-    user_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': "Ім'я:"}))
-    phone_number = forms.IntegerField(label='', widget=forms.TextInput(attrs={'placeholder': 'Номер телефону:'}))
-    user_email = forms.EmailField(label='', widget=forms.TextInput(attrs={'placeholder': 'E-Mail:'}))
-    user_message = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': 'Повідомлення:'}))
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
--- botanical_garden_root/contact/models.py
+++ botanical_garden_root/contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    user_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    user_email = models.EmailField()
+    user_message = models.CharField(max_length=1000)

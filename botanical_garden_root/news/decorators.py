1faf86a9f8086aac67bb6f425971ab44a9921b22
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-05-09 08:01:26 +0300

    now loop through all groups and created template for permision denied page

--- botanical_garden_root/news/decorators.py
+++ botanical_garden_root/news/decorators.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
@@ -6,12 +6,14 @@ def allowed_users(allowed_roles=[]):
             
             group = None
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
+                group = request.user.groups.all()
 
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse('Куда собрался?')
+
+            for i in group:
+                if i.name in allowed_roles:
+                    return view_func(request, *args, **kwargs)
+                
+            return render(request, 'permision_denied.html')
 
         return wrapper_func
     return decorator

b488436284393f90e6843d55f03b2af452c84733
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-05-12 09:06:51 +0300

    add validators to excursions

--- botanical_garden_root/excursion/models.py
+++ botanical_garden_root/excursion/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+from datetime import date
 from accounts.models import Customer
 
 EXCURSION_TYPE_CHOICES = [
@@ -13,6 +14,19 @@ EXCURSION_TYPE_CHOICES = [
 ]
 
 def validate_date(value):
+    today = date.today()
+    if value < today:
+        raise ValidationError(
+            _('Ми не працюємо в минулому'),
+            params={'value':value}
+        )
+
+    if value == today:
+        raise ValidationError(
+            _('Неможливо записатися на сьогодні'),
+            params={'value':value}
+        )
+
     if value.weekday() in [5, 6]:
         raise ValidationError(
             _('В цей день у нас вихідний'),
@@ -21,18 +35,32 @@ def validate_date(value):
 
 
 def validate_time(value):
-    if not value:
+    if  value.hour < 8:
         raise ValidationError(
-            _(f'Записатися на {type(value)} годину неможливо'),
+            _(f'Записатися на {value.hour} годину неможливо, занадто рано'),
             params={'value':value}
         )
+
+    if  value.hour >= 15 and value.minute != 0:
+        raise ValidationError(
+            _(f'Записатися на {value.hour} годину неможливо, занадто пізно'),
+            params={'value':value}
+        )
+
+    if  value.hour == 8 and value.minute < 30:
+        raise ValidationError(
+            _(f'Записатися на {value.hour}:{value.minute} неможливо, занадто рано'),
+            params={'value':value}
+        )
+    
+
 
 class Excursion(models.Model):
     person = models.ForeignKey(Customer, blank=True, null=True ,on_delete=models.CASCADE)
     excursion_date = models.DateField(null=True, validators=[validate_date])
     excursion_time = models.TimeField(null=True, validators=[validate_time])
     excursion_type = models.CharField(max_length=100, choices=EXCURSION_TYPE_CHOICES)
-
-
+    
+    
     def __str__(self):
         return self.person.user.username

03acf2dbb93b4ca9bb5f73610123c8599e1d4fe5
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-06-04 06:33:30 +0300

    security key removed

--- botanical_garden_root/botanical_garden/settings.py
+++ botanical_garden_root/botanical_garden/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#^^2ts35+y1xk^&c(3#cc3!)-au_+@pos@v6v@2cmz@&$!cw3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

b2a8c0dc534142f933b17e256aa892355857576e
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-06-26 10:01:57 +0300

    adjusted template and url for new view

--- excursion/urls.py
+++ excursion/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.excursions, name='excursions'),
     path('all', views.excursions_all, name='excursions_all'),
     path('delete/<str:excursions_pk>', views.excursions_delete, name='delete_excursion'),
+    path('delete/<str:excursions_pk>/', views.excursions_delete_only, name='delete_excursion_only'),
 ]

4778d84a963d5b4718ddc3a4ca48a06eb9a7fd9e
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-07-09 14:16:52 +0300

    news endpoint

--- news/urls.py
+++ news/urls.py
@@ -1,1 +1,11 @@
-# urls
+from django.urls import path, include
+from rest_framework import routers
+
+from news import views
+
+router = routers.DefaultRouter()
+router.register(r'news', views.NewsViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- news/views.py
+++ news/views.py
@@ -1,1 +1,10 @@
-# views
+from rest_framework import viewsets, permissions
+
+from .models import News
+from .serializers import NewsSerializer
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    permission_classes = [permissions.IsAuthenticated]

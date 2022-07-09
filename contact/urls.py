02526843e39148b953edfd67af6e3930e9607310
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-07-09 14:16:03 +0300

    endpoint for contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,1 +1,12 @@
-# urls
+from django.urls import path, include
+from rest_framework import routers
+
+from contact import views
+
+
+router = routers.DefaultRouter()
+router.register(r'contacts', views.ContactViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,10 @@
-# views
+from rest_framework import viewsets, permissions
+
+from .models import Contact
+from .serializers import ContactSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.IsAuthenticated]

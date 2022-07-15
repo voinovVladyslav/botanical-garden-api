87a793aaf6282f327d27317d6045aa2a99b49bbb
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-07-15 15:26:31 +0300

    created custom permission classes

--- botanical_garden/permissions.py
+++ botanical_garden/permissions.py
@@ -13,3 +13,46 @@ class IsManagerOrReadOnly(permissions.BasePermission):
         manager = Group.objects.filter(name='manager').first()
         
         return manager == user_groups
+
+
+class IsAuthorOrIsManagerOrReadOnly(permissions.BasePermission):
+    message = 'Only manager can view all messages'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        user_groups = request.user.groups.filter(name='manager').first()
+        manager = Group.objects.filter(name='manager').first()
+        is_manager = manager == user_groups
+
+        return request.user == obj.user or is_manager 
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    message = "You can't modify this, you're not the author"
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.user
+
+class ReadOnly(permissions.BasePermission):
+    message = "Read only"
+
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return False
+        return True 
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return False
+
+
+class OnlySelf(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj

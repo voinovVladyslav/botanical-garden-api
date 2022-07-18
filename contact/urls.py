0160ef9fca129c1cc7720dcf769772acbe340b15
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-07-18 15:22:54 +0300

    CRUD functionality on contacts

--- contact/urls.py
+++ contact/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from contact import views
 
-
-
+# api/contacts/
 urlpatterns = [
     path('', views.contacts, name='contacts'),
+    path('create', views.contact_create, name='contact_create'),
     path('<str:pk>/', views.contact_detail, name='contact_detail'),
+    path('<str:pk>/update', views.contact_update, name='contact_update'),
+    path('<str:pk>/delete', views.contact_delete, name='contact_delete'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from contact import serializers
 
 from contact.models import Contact
 from contact.serializers import ContactSerializer
@@ -25,3 +26,50 @@ def contact_detail(request, pk):
     
     serializer = ContactSerializer(contact, context={'request':request})
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def contact_create(request):
+    user = User.objects.first()
+    contact = Contact(user=user)
+    serializer = ContactSerializer(contact, data=request.data, context={'request':request})
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def contact_update(request, pk):
+    try:
+        contact = Contact.objects.get(pk=pk)
+    except:
+        data = {'error':'this contact does not exists'}
+        return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ContactSerializer(contact, data=request.data, context={'request':request})
+    data = {}
+    if serializer.is_valid():
+        serializer.save()
+        data['success'] = 'successfuly updated'
+        return Response(data=data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def contact_delete(request, pk):
+    try:
+        contact = Contact.objects.get(pk=pk)
+    except:
+        data = {'error':'this contact does not exists'}
+        return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+
+    operation = contact.delete()
+    data = {}
+    if operation:
+        data['success'] = 'successfuly deleted'
+    else:
+        data['error'] = 'delete failed'
+    return Response(data=data)
+    

e82f078fbd71c5057827482a4ce47f833ecd9ddb
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-08-28 14:40:02 +0300

    add custom user model + migrations

--- botanical-garden-api/core/models.py
+++ botanical-garden-api/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_manager(self, email, password, **extra_fields):
+        manager = self.create_user(email, password, **extra_fields)
+        manager.is_manager = True
+        manager.save(using=self._db)
+
+        return manager
+
+    def create_superuser(self, email, password, **extra_fields):
+        superuser = self.create_user(email, password, **extra_fields)
+        superuser.is_manager = True
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.save(using=self._db)
+
+        return superuser
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_manager = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'

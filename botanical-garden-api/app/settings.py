403ba4261ac4329e6bb1b9874e9de16d74380705
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-08-28 15:36:21 +0300

    add auto api documentation

--- botanical-garden-api/app/settings.py
+++ botanical-garden-api/app/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     #local
     'core',
+
+    #packages
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}

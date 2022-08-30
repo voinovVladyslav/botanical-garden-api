f521925ead912589b67b9a68cd98b8f709d0a719
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-08-30 14:33:11 +0300

    tests for profile endpoint

--- botanical-garden-api/user/tests/test_user_api.py
+++ botanical-garden-api/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -106,3 +107,57 @@ class UserApiTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', res.data)
+
+    def test_update_user_auth_required(self):
+        user_data = {
+            'email': 'test@example.com',
+            'password': 'testpassword123',
+            'first_name': 'Dima',
+            'last_name': 'Tsal',
+        }
+        create_user(**user_data)
+
+        data = {
+            'first_name': 'Pasha',
+        }
+        res = self.client.patch(ME_URL, data)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthorizedUserApiTests(TestCase):
+    def setUp(self):
+        self.user = create_user(
+            email='test@example.com',
+            password='testpassword123',
+            first_name='First name',
+            last_name='Last name',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_profile(self):
+        res = self.client.get(ME_URL)
+
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.last_name)
+        self.assertNotContains(res, 'password')
+
+    def test_post_not_allowed(self):
+        res = self.client.post(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_profile(self):
+        data = {
+            'first_name': 'New name',
+            'password': 'newstrongpass',
+        }
+
+        res = self.client.patch(ME_URL, data)
+
+        self.user.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.first_name, data['first_name'])
+        self.assertTrue(self.user.check_password(data['password']))

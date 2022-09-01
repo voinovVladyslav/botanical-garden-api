c130d9f10060235ad8b8fc1ba3520d67998d32a5
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-01 10:23:22 +0300

    add excursion model

--- botanical-garden-api/excursion/models.py
+++ botanical-garden-api/excursion/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.conf import settings
 
-# Create your models here.
+
+class Excursion(models.Model):
+    class ExcursionType(models.TextChoices):
+        CHILD = 'CH', _('Child')
+        ADULT = 'AD', _('Adult')
+        FILMING = 'FI', _('Filming')
+        EVENT_HOLDING = 'EH', _('Event holding')
+        SELF_EXCURSION = 'SE', _('Self excursion')
+
+    type = models.CharField(
+        max_length=2,
+        choices=ExcursionType.choices,
+        default=ExcursionType.SELF_EXCURSION,
+    )
+    date = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+

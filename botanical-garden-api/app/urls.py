021c235be445b9ca3a66c9c6b2857e432d06f6fb
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-02 17:12:29 +0300

    configure static data

--- botanical-garden-api/app/urls.py
+++ botanical-garden-api/app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -24,3 +26,9 @@ urlpatterns = [
     path('api/excursion/', include('excursion.urls')),
     path('api/news/', include('news.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )

64e90d35706a0d51e2c70d5d6ebaedad2a33f291
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-02 18:04:26 +0300

    add upload news image endpoint

--- botanical-garden-api/news/serializers.py
+++ botanical-garden-api/news/serializers.py
@@ -11,5 +11,13 @@ class NewsSerializer(serializers.ModelSerializer):
 
 class NewsDetailSerializer(NewsSerializer):
     class Meta(NewsSerializer.Meta):
-        fields = NewsSerializer.Meta.fields + ['context', 'publication_date']
+        fields = NewsSerializer.Meta.fields + ['context', 'publication_date', 'image']
         read_only_fieds = NewsSerializer.Meta.read_only_fieds + ['publication_date']
+
+
+class NewsImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
--- botanical-garden-api/news/views.py
+++ botanical-garden-api/news/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 from news.models import News
 from news.serializers import (
     NewsSerializer,
     NewsDetailSerializer,
+    NewsImageSerializer,
 )
 from news.permissions import IsManagerOrReadOnly
 
@@ -24,4 +28,17 @@ class NewsViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'list':
             return NewsSerializer
+        elif self.action == 'upload_image':
+            return NewsImageSerializer
         return self.serializer_class
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        news = self.get_object()
+        serializer = self.get_serializer(news, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

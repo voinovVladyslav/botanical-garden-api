6877b44ba3bf5fd93f80d2a1b1b8466efb51bdd2
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-10 14:34:02 +0300

    add hashtag model

--- botanical-garden-api/news/admin.py
+++ botanical-garden-api/news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from news.models import News
+from news.models import News, Hashtag
 
 
 admin.site.register(News)
+admin.site.register(Hashtag)
--- botanical-garden-api/news/models.py
+++ botanical-garden-api/news/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 
 import uuid
 import os
+
+
+def validate_hashtag(value):
+    if not value.startswith('#'):
+        raise ValidationError(
+            _('Hashtag must start with #'),
+            params={'value': value}
+        )
 
 
 def news_image_file_path(instance, filename):
@@ -15,10 +25,17 @@ def news_image_file_path(instance, filename):
 class News(models.Model):
     title = models.CharField(max_length=255)
     context = models.TextField()
-    hashtag = models.CharField(max_length=100)
+    hashtag = models.ManyToManyField('Hashtag')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     publication_date = models.DateField(auto_now_add=True)
     image = models.ImageField(null=True, upload_to=news_image_file_path)
 
     def __str__(self):
         return self.title
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=100, validators=[validate_hashtag])
+
+    def __str__(self):
+        return self.name
